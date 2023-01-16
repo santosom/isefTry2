a3b97aa8f7cf29267d64e662961829cef5c1fbe7
madelinesantoso <santosom@wlhs.wlwv.k12.or.us>
2023-01-16 13:12:18 -0800

    imported some libaries

--- main.py
+++ main.py
@@ -7,6 +7,13 @@ from tensorflow.keras.utils import to_categorical
 import pandas as pd
 import numpy as np
 import librosa
-#glob comes with python installed, so you don't need to install it specifically using pip install or in the python interpreter tab
-import glob
+from pathlib import Path
+from scipy.io import wavfile
+"""glob comes with python installed, so you don't need to install it specifically using pip install or in the python interpreter tab"""
+from glob import glob
 print("successful")
+#preprocessing
+"""snoring_files = glob("SnoringNoises/*.wav")
+snoring_file_name = ".\Datasets\Snoring Dataset\SnoringNoises\1_18.wav"
+snore = wavfile.read(snoring_file_name)
+print(snore)"""

2619ceb1b21bba5e701dbd1f73177be22b2472bc
madelinesantoso <santosom@wlhs.wlwv.k12.or.us>
2023-01-16 11:50:15 -0800

    Initial commit

---
+++ main.py
@@ -0,0 +1,12 @@
+import tensorflow
+from tensorflow import keras
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.utils import to_categorical
+
+import pandas as pd
+import numpy as np
+import librosa
+#glob comes with python installed, so you don't need to install it specifically using pip install or in the python interpreter tab
+import glob
+print("successful")

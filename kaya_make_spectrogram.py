78d60686cb64b7e0b9ab443b7237064e8a9ec549
HaldTPK <haldk@wlhs.wlwv.k12.or.us>
2023-01-22 23:33:07 -0800

    working spectrogram generation

--- kaya_make_spectrogram.py
+++ kaya_make_spectrogram.py
@@ -9,27 +9,29 @@ import numpy as np
 
 # get a command line argument
 
-# # if the argv array is empty exit with an error
-# if len(sys.argv) < 2:
-#     print('Please enter a file name')
-#     sys.exit()
-#
-# # get a commnad line parameter for the file we want to open
-# file = sys.argv[1]
-#
-# # if the file doesn't exist exit with an error
-# if not os.path.exists(file):
-#     print('File does not exist')
+# if the argv array is empty exit with an error
+if len(sys.argv) < 2:
+    print('Please enter a file name')
+    sys.exit()
 
-def createSpectrogram(file):
+# get a commnad line parameter for the file we want to open
+file = sys.argv[1]
+
+# if the file doesn't exist exit with an error
+if not os.path.exists(file):
+    print('File does not exist')
+
+filename = os.path.basename(file)
+
+def createSpectrogram(filename, file):
     y, sr = librosa.load(file)
     plt.figure(figsize=(14, 5))
     librosa.display.specshow(librosa.amplitude_to_db(librosa.stft(y), ref=np.max), y_axis='log', x_axis='time')
     plt.colorbar(format='%+2.0f dB')
-    plt.title('Power spectrogram')
+    # plt.title('Power spectrogram')
     plt.tight_layout()
-    plt.savefig('spectrograms/' + file + '.png')
+    plt.savefig('spectrograms/' + filename + '.png')
 
 # make a spectrogram image of the audio file
 # import the necessary packages
-# createSpectrogram(file)
+createSpectrogram(filename, file)

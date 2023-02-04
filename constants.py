08e5ed2b458261ec7dbfc193eb00abc3432cd69d
HaldTPK <haldk@wlhs.wlwv.k12.or.us>
2023-02-03 22:02:12 -0800

    working spectrogram generation

--- constants.py
+++ constants.py
@@ -1,10 +1,42 @@
+import numpy as np
 import tensorflow as tf
 
 #change this back to actual spectrograms
-snoringFilePath = 'test_spectrograms/snoring'
-ambulanceFilePath = 'test_spectrograms/alarm'
+snoringFilePath = 'test_spectrograms/snoring/*png'
+ambulanceFilePath = 'test_spectrograms/alarm/*png'
 snoringData = tf.data.Dataset.list_files(snoringFilePath)
 ambulanceData = tf.data.Dataset.list_files(ambulanceFilePath)
+
+image_size = (180, 180)
+batch_size = 128
+
+train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
+    "test_spectrograms",
+    validation_split=0.2,
+    subset="both",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+print ("test data: ")
+print (train_ds)
+
+import matplotlib.pyplot as plt
+
+plt.figure(figsize=(10, 10))
+for images, labels in train_ds.take(1):
+    print (labels)
+    print (images)
+    for i in range(4):
+        print (images[i])
+        ax = plt.subplot(3, 3, i + 1)
+        plt.imshow(images[i].numpy().astype("uint8"))
+        plt.title(int(labels[i]))
+        plt.axis("off")
+        plt.savefig('all.png')
+
+print ("done")
 
 snoringLen = len(snoringData)
 ambulanceLen = len(ambulanceData)
--- main.py
+++ main.py
@@ -22,24 +22,35 @@ ambulanceData = constants.ambulanceData
 print("hello and welcome to the clown show")
 #label snoring data by creating another tensor filled entirely with "0", to the proportion of snoringdata, and slapping (zipping) it together
 #Currently, snoring's label is 0, ambulance's label is 1 (update as needed)
-labelledSnoringData = tf.data.Dataset.zip((snoringData, tf.data.Dataset.from_tensor_slices(tf.fill(len(snoringData), 0)) ))
-labelledAmbulanceData = tf.data.Dataset.zip((ambulanceData, tf.data.Dataset.from_tensor_slices(tf.fill(len(ambulanceData), 1)) ))
-allLabelledDatasets = [labelledSnoringData, labelledAmbulanceData]
+# labelledSnoringData = tf.data.Dataset.zip((snoringData, tf.data.Dataset.from_tensor_slices(tf.fill(len(snoringData), 0)) ))
+
+# print out labelledSnoringData
+# print("labelledSnoringData: ", labelledSnoringData)
+# print ("labelledSnoringData type: ", type(labelledSnoringData))
+# print out the tensor of the first element in labelledSnoringData
+# print("labelledSnoringData[0]: ", labelledSnoringData[0])
+
+# labelledAmbulanceData = tf.data.Dataset.zip((ambulanceData, tf.data.Dataset.from_tensor_slices(tf.fill(len(ambulanceData), 1)) ))
+# allLabelledDatasets = [labelledSnoringData, labelledAmbulanceData]
 #congegate all datasets together, note maybe create a for-loop when using more than 2 datasets. this is going to get annoying
-ultimateDataset = labelledSnoringData.concatenate(labelledAmbulanceData)
-print("dataset type: ", type(ultimateDataset))
+# ultimateDataset = labelledSnoringData.concatenate(labelledAmbulanceData)
+# print("dataset type: ", type(ultimateDataset))
 
 #TODO: implement something to split up training and testing sets. I'm not sure if k-fold validation applies here but I'd like to use it somewhere
 
-ultimateDataset.shuffle(3, seed=None, reshuffle_each_iteration=False)
-print("dataset shuffled")
-ultimateDataset = ultimateDataset.batch(2)
-ultimateDataset = ultimateDataset.prefetch(2)
-print("dataset batched and prefetched")
-train = ultimateDataset.take(constants.trainingLen)
-print("part of dataset taken")
-test = ultimateDataset.skip(constants.trainingLen).take(constants.testingLen)
-print("dataset split into validation, training, and testing")
+# ultimateDataset.shuffle(3, seed=None, reshuffle_each_iteration=False)
+# print("dataset shuffled")
+# ultimateDataset = ultimateDataset.batch(2)
+# ultimateDataset = ultimateDataset.prefetch(2)
+# print("dataset batched and prefetched")
+# train = ultimateDataset.take(constants.trainingLen)
+# print("part of dataset taken")
+# # print train
+# print ("train: ", train)
+# test = ultimateDataset.skip(constants.trainingLen).take(constants.testingLen)
+# print("dataset split into validation, training, and testing")
+# # print test
+# print ("test: ", test)
 #make model (woah)
 model = Sequential()
 #first layer
@@ -61,12 +72,12 @@ model.compile(optimizer, loss='BinaryCrossentropy', metrics=[tf.keras.metrics.Re
 print("model successfully complied")
 
 model.fit(
-    x=ultimateDataset,
+    x=constants.testData,
     y=None,
     batch_size=3,
     epochs=5,
     verbose=1,
-    validation_data=test)
+    validation_data=constants.testData)
 print("model fit completed")
 model.build(((constants.entireLen()), imageheight, imagewidth, 3))
 print(model.summary())

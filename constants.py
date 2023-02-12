bce94db774704e5f70adb16f15fa24a41c0ef772
madelinesantoso <santosom@wlhs.wlwv.k12.or.us>
2023-02-11 20:31:22 -0800

    it runs

--- constants.py
+++ constants.py
@@ -10,10 +10,19 @@ ambulanceData = tf.data.Dataset.list_files(ambulanceFilePath)
 image_size = (1400, 500)
 batch_size = 2
 
-train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
+train_ds = tf.keras.utils.image_dataset_from_directory(
     "test_spectrograms",
     validation_split=0.2,
-    subset="both",
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
+
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "test_spectrograms",
+    validation_split=0.2,
+    subset="validation",
     seed=1337,
     image_size=image_size,
     batch_size=batch_size,
@@ -25,15 +34,16 @@ train_ds, val_ds = tf.keras.utils.image_dataset_from_directory(
 import matplotlib.pyplot as plt
 
 plt.figure(figsize=(10, 10))
-for images, labels in train_ds.take(1):
-    print (labels)
-    print (images)
-    # for i in range(4):
-    #     ax = plt.subplot(3, 3, i + 1)
-    #     plt.imshow(images[i].numpy().astype("uint8"))
-    #     plt.title(int(labels[i]))
-    #     plt.axis("off")
-    #     plt.savefig('all.png')
+# for images, labels in train_ds.take(1):
+#     print (labels)
+#     print (images)
+#     # for i in range(4):
+#     #     ax = plt.subplot(3, 3, i + 1)
+#     #     plt.imshow(images[i].numpy().astype("uint8"))
+#     #     plt.title(int(labels[i]))
+#     #     plt.axis("off")
+#     #     plt.savefig('all.png')
+#
 
 print ("done")
 
--- main.py
+++ main.py
@@ -2,9 +2,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten,Conv2D, Dense, MaxPooling2D
+from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten,Conv2D, Dense, MaxPooling2D, Input
 from keras.optimizers import Adam
-from tensorflow.keras.datasets import mnist
 
 import pandas as pd
 import numpy as np
@@ -14,42 +13,67 @@ from scipy.io import wavfile
 from sklearn.model_selection import KFold
 from matplotlib import pyplot as plt
 
-import constants
+image_size = (1400, 500)
+batch_size = 2
+print("hello mushy")
+train_ds = tf.keras.utils.image_dataset_from_directory(
+    "test_spectrograms",
+    label_mode="categorical",
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
 
-imageheight, imagewidth = 1400, 500
-snoringData = constants.snoringData
-ambulanceData = constants.ambulanceData
-(X_train, Y_train), (X_test, Y_test) = mnist.load_data()
-print("test shape = ", constants.train_ds.shape())
+val_ds = tf.keras.utils.image_dataset_from_directory(
+    "test_spectrograms",
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    image_size=image_size,
+    batch_size=batch_size,
+)
 
-x_shape = 0, 28, 28
-input_shape = (1400, 500, 3)
+for images, labels in train_ds.take(1):
+    print (labels)
+    print (images)
+
+
+print("training stuff defined")
+print("class names are ", train_ds.class_names)
+print ("x_train shape before messing with stuff:", train_ds)
+print (len(train_ds), "train samples")
+print (len(val_ds), "test samples")
+
 
 model = Sequential()
-model.add(Conv2D(32, (3, 3), padding="same", activation="relu", input_shape = (28, 28, 1)))
+model.add(Conv2D(32, (3, 3), padding="same", activation="relu", input_shape = (1400, 500, 3)))
 model.add(Conv2D(32, (3, 3), padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Conv2D(32, (3, 3), padding="same", activation="relu"))
 model.add(Conv2D(32, (3, 3), padding="same", activation="relu"))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dense(64, activation="relu"))
+model.add(Flatten())
 model.add(Dense(32, activation="relu"))
+model.add(Dense(2, activation = 'softmax'))
 model.summary()
 print("model layers defined")
 optimizer = keras.optimizers.Adam(learning_rate=0.01)
-# # print("model layers optimizer defined")
-model.compile(optimizer, loss='categorical_crossentropy', metrics=['accuracy']) #change to categorical_crossentropy
-# print("model successfully complied")
+print("model layers optimizer defined")
+model.compile(optimizer, loss='binary_crossentropy', metrics=['accuracy']) #change to categorical_crossentropy
+print("model successfully complied")
 
-m = model.fit(
-    # constants.train_ds,
-    X_train,
-    Y_train,
-    batch_size=3,
-    epochs=5,
-    verbose=1,
-    validation_data=constants.val_ds
-)
+model.fit(train_ds, batch_size = 3, epochs = 5, verbose = 1)
+# m = model.fit(
+#     # constants.train_ds,
+#     X_train,
+#     Y_train,
+#     batch_size=3,
+#     epochs=5,
+#     verbose=1,
+# )
 
 print("model fit completed")
 # model.build(((constants.entireLen()), imageheight, imagewidth, 3))
